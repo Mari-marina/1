6cda749f14779a0397509bdee78ccb2bbac870db
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2022-08-04 00:21:26 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,16 +19,31 @@ if (document.querySelector(".icon-menu")) {
   });
 }
 
+
+// if (document.querySelector(".main__wrapper")) {
+//   document.querySelector(".main__wrapper").addEventListener("click", function (e) {
+//     document.querySelector("body").classList.toggle("lock");
+//     document.documentElement.classList.toggle("menu-open");
+//   });
+// }
+
 const mm = document.querySelector(".mm");
 const form1 = document.querySelector(".request__form-mobile");
 const form2 = document.querySelector(".request__form-mobile.two");
 const formBtn = document.querySelector(".mobile__BTNtext");
 const formBtn2 = document.querySelector(".mobile__BTNtext.two");
+// const navigation = document.querySelector(".main__wrapper");
+
 
 mm.addEventListener("click", function (e) {
   document.querySelector("body").classList.remove("lock");
   document.documentElement.classList.remove("menu-open");
 });
+
+// navigation.addEventListener("click", function (e) {
+//   document.querySelector("body").classList.remove("lock");
+//   document.documentElement.classList.remove("menu-open");
+// });
 
 formBtn.addEventListener("click", function (e) {
   form2.classList.remove("active");
@@ -38,5 +53,8 @@ formBtn2.addEventListener("click", function (e) {
   form2.classList.add("active");
   form1.classList.remove("active");
 });
+
+
+
 
 //# sourceMappingURL=script.js.map
